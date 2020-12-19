ba7d682d4309fced961027b858ef501d9f627a51
aaronmck <aaronatwpi@gmail.com>
2020-12-19 08:55:12 -0500

    working v1

--- src/test/scanner_plus_door.py
+++ src/test/scanner_plus_door.py
@@ -1,6 +1,8 @@
 from bluepy.btle import Scanner
 import pi_servo_hat
 import smbus, time
+import datetime
+import logging
 
 bus = smbus.SMBus(1)
 addr = 0x40
@@ -22,36 +24,64 @@ time.sleep(.25)
 #bus.write_word_data(addr, 0x08, 460) # chl 0 end time = 2.0ms (90+ degrees, fully closed)
 # Initialize Constructor
 
-current_position = 0
-open_pos = 50
-closed_pos = 460
-jumps = 20
-rssi_threshold = -50
-rssi_buffer_size = 4
-scan_spacing_in_secs = 1.0
+rssi_threshold = -47
+rssi_buffer_size = 2
+scan_spacing_in_secs = 0.5
+seconds_door_stays_open = 30
 
+logfile=open("Logfile.txt","w")
+
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+logging.info("""Dog door monitor v1""")
+
+
+epoch = datetime.datetime.utcfromtimestamp(0)
+
+def unix_time_millis(dt):
+    return (dt - epoch).total_seconds() * 1000.0
 class MotorDoor:
     def __init__(self,swing_range=110):
-        self.open_position = 209
-        self.closed_position = 460
-        self.jump_dist = 20
+        self.open_position = 240
+        self.closed_position = 420
+        self.jump_dist = 5
         self.swing_range = swing_range
 
-        self.rotation_position = 0
-        self.move_to(0)
-        self.is_open = True
-        
+        self.rotation_position = self.closed_position
+        self.move_to(self.closed_position)
+        self.opened_time = datetime.datetime.now().timestamp() - 100
+        self.is_open = False
+
     def slow_open(self):
         if not self.is_open:
-            self.move_to(self.open_position)
+            logging.info("Opening door")
+            for i in range(self.closed_position,self.open_position,-1 * self.jump_dist):
+                self.move_to(i)
+                time.sleep(0.1)
             self.rotation_position = self.open_position
+            self.opened_time = datetime.datetime.now().timestamp()
+        else:
+            self.move_to(self.open_position) # keep forcing open
         self.is_open = True
         
     def slow_close(self):
         if self.is_open:
-            self.move_to(self.closed_position)
-            self.rotation_position = self.closed_position
-        self.is_open = False
+            tm = datetime.datetime.now().timestamp()
+            if tm - self.opened_time > 30:
+                logging.info("Closing door")
+                for i in range(self.open_position,self.closed_position,self.jump_dist):
+                    self.move_to(i)
+                    time.sleep(0.1)
+                self.move_to(self.closed_position)
+                self.rotation_position = self.closed_position
+                self.is_open = False
+            else:
+                logging.info("Door not closed due to timeout")
+        else:
+            self.move_to(self.closed_position) # keep forcing open
         
     def move_to(self,move_to_position):
         #write start and stop to channel
@@ -65,20 +95,20 @@ scanner = Scanner()
 
 scan_buffer = [-100 for i in range(rssi_buffer_size)]
 
-for i in range(0,100):
+while True:
     devices = scanner.scan(scan_spacing_in_secs)
     
     for device in devices:
         if device.addr == 'd5:dd:6b:2b:66:c1':
-            print(device.rssi)
             scan_buffer = scan_buffer[1:len(scan_buffer)]
             scan_buffer.append(device.rssi)
             avg = float(sum(scan_buffer))/float(len(scan_buffer))
+            logging.info("DEV = {}  RSSI = {}  Average = {}  DOOR_OPEN = {}".format(device.addr, device.rssi,avg,door.is_open))
+            logfile.write("DEV = {}  RSSI = {}  Average = {}  DOOR_OPEN = {}".format(device.addr, device.rssi,avg,door.is_open))
+            
             if avg < rssi_threshold:
-                print("FAR")
                 door.slow_close()
             else:
-                print("CLOSE")
                 door.slow_open()
 
-                print("DEV = {} RSSI = {}".format(device.addr, device.rssi))
+ 
