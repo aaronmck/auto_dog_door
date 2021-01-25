1c7b76c0ffb5b9fe2b402216efb004105fc8685c
aaronmck <aaronatwpi@gmail.com>
2021-01-24 21:51:01 -0500

    typo

--- src/ir_based/main.py
+++ src/ir_based/main.py
@@ -42,7 +42,7 @@ class DoorState:
     def __init__(self,door,bus,start_open=False,min_open_dist=90,min_open_secs=30):
         self.door = door
         self.door_state = Door.Closed
-        if start_open Door.HOLD_OPEN:
+        if start_open == Door.HOLD_OPEN:
             self.door_state = Door.CLOSED
         self.door_position_open = start_open
         self.min_open_secs = min_open_secs
